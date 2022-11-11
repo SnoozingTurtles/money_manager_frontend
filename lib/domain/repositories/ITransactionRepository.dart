1112a2e91a6644f4182bf268109b3f8baf60ef25
squidrye <mihirs167@gmail.com>
2022-11-12 03:34:13 +0530

    minor changes

--- lib/domain/repositories/ITransactionRepository.dart
+++ lib/domain/repositories/ITransactionRepository.dart
@@ -1,7 +1,7 @@
 import 'package:money_manager/domain/models/transaction_model.dart';
 
 abstract class ITransactionRepository{
-  void add(Transaction transactions);
+  Future<void> add(Transaction transactions);
   //TODO: Update method
-  List<Transaction> get();
+  Future<List<Transaction>> get();
 }
